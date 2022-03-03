41b8c15d0bf51b57815d329331f7efc8c017636c
Alex Lieberman <alexlieberman55@hotmail.com>
2022-03-02 21:17:36 -0700

    created buildTable function

--- static/java/app.js
+++ static/java/app.js
@@ -1,3 +1,21 @@
 const tableData = data;
 var tbody = d3.select("tbody");
 
+function buildTable(data){
+    // clear existing table data
+    tbody.html("");
+
+    // loop through each object
+    data.forEach(dataRow => {
+        // append a row to the table
+        let row = tbody.append("tr");
+
+        // loop though each field 
+        Object.values(dataRow).forEach((val) => {
+            // append each value as a table cell
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    });
+}
+
